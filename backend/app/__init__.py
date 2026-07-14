7507c5df795fc1891a0c9091b2bb35b97f7bffdd
BlackCoder56 <blackcoda56@gmail.com>
2026-07-14 14:55:58 +0300

    Authentication blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.auth import auth_bp
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -8,6 +9,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    app.register_blueprint(auth_bp)
 
     db.init_app(app)
     migrate.init_app(app, db)
