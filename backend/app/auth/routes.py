236f89ff3bc2d3cc99f5df755c78d8d5d0443a55
BlackCoder56 <blackcoda56@gmail.com>
2026-07-16 10:39:03 +0300

    Validate required fields

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -5,4 +5,14 @@ from app.auth import auth_bp
 def register():
     data = request.get_json()
 
-    return jsonify(data), 200
+    required_fields = ["username", "email", "password"]
+
+    for field in required_fields:
+        if not data.get(field):
+            return jsonify({
+                "message": f"{field} is required."
+            }), 400    
+
+    return jsonify({
+        "message": "Validation passed!"
+    }), 200
